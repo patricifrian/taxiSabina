05a818dbbd68b37c5f9ee52a2230e75f25eec7a2
patricifrian <patricifrian@gmail.com>
2024-07-31 00:19:53 +0200

    Update

---
+++ script.js
@@ -0,0 +1,16 @@
+document.getElementById("aboutBtn").addEventListener("click", function () {
+    loadContent("about.html");
+});
+
+document.getElementById("contactBtn").addEventListener("click", function () {
+    loadContent("contact.html");
+});
+
+function loadContent(page) {
+    fetch(page)
+        .then(response => response.text())
+        .then(data => {
+            document.getElementById("content").innerHTML = data;
+        })
+        .catch(error => console.log('Error:', error));
+}
